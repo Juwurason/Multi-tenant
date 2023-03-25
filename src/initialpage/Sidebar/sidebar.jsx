f8204b0349681bfd4648f3b906cff6b1ab966456
Tobby Makinde <makinde.tobiloba@gmail.com>
2023-03-25 15:04:49 +0100

    New Changes

--- src/initialpage/Sidebar/sidebar.jsx
+++ src/initialpage/Sidebar/sidebar.jsx
@@ -430,12 +430,13 @@ const Sidebar = (props) => {
                 <span>Main</span>
               </li>
               <li className="submenu">
-                <a href="#" className={isSideMenu == "dashboard" ? "subdrop" : ""} onClick={() => toggleSidebar(isSideMenu == "dashboard" ? "" : "dashboard")}><i className="la la-dashboard" /> <span> Dashboard</span> <span className="menu-arrow" /></a>
+                <a href="#" className={isSideMenu == "dashboard" ? "subdrop" : ""} onClick={() => toggleSidebar(isSideMenu == "dashboard" ? "" : "dashboard")}><i className="la la-dashboard" />
+                  <span> Dashboard</span> <span className="menu-arrow" /></a>
                 {isSideMenu == "dashboard" ?
                   <ul >
                     <li><Link className={pathname.includes('main/dashboard') ? "active" : ""} to="/app/main/dashboard">Admin Dashboard</Link></li>
-                    <li><Link className={pathname.includes('main/employee-') ? "active" : ""}
-                      to="/app/main/employee-dashboard">Employee Dashboard</Link></li>
+                    {/* <li><Link className={pathname.includes('main/employee-') ? "active" : ""}
+                      to="/app/main/employee-dashboard">Employee Dashboard</Link></li> */}
                   </ul>
                   : ""
                 }
