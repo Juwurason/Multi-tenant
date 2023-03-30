5cbbe1de5c772ab167610107c960206e5e57cd31
Juwurason <ajibolasunday7@gmail.com>
2023-03-30 12:59:37 +0100

    details added to editProfile

--- src/context/index.jsx
+++ src/context/index.jsx
@@ -8,9 +8,11 @@ export const useCompanyContext = () => useContext(CompanyContext);
 export const CompanyProvider = ({ children }) => {
     const [staff, setStaff] = useState([]);
     const [staffNum, setStaffNum] = useState(0);
+    const [staffProfile, setStaffProfile] = useState({});
 
+    const getStaffProfile = JSON.parse(localStorage.getItem('staffProfile'))
     const privateHttp = useHttp();
-
+    
     useEffect(() => {
         let isMounted = true;
         async function FetchStaff() {
@@ -19,13 +21,14 @@ export const CompanyProvider = ({ children }) => {
             try {
                 const response = await Promise.all([
                     privateHttp.get('/Staffs'),
-
+                    privateHttp.get(`/Staffs/${getStaffProfile.staffId}`)
 
                 ])
                 const staff = response[0].data;
+                const staffPro = response[1].data;
                 setStaff(staff)
                 setStaffNum(staff.length)
-
+                setStaffProfile(staffPro)
 
             } catch (error) {
                 console.log(error);
@@ -80,7 +83,7 @@ export const CompanyProvider = ({ children }) => {
         companyId, email, storeCompanyId,
         storeAdminEmail, clearCompanyData,
         userProfile, setUserProfile, staffNum, setStaffNum,
-        staff, setStaff
+        staff, setStaff, staffProfile
     };
 
     return (
