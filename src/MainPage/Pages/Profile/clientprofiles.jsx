e7de9c334f814c2e33693dbb5d685c7899d9c254
Juwurason <ajibolasunday7@gmail.com>
2023-07-28 02:56:42 +0100

    client roster added in admin

--- src/MainPage/Pages/Profile/clientprofiles.jsx
+++ src/MainPage/Pages/Profile/clientprofiles.jsx
@@ -10,10 +10,27 @@ import Offcanvas from '../../../Entryfile/offcanvance';
 import useHttp from '../../../hooks/useHttp'
 import man from '../../../assets/img/man.png'
 import { toast } from 'react-toastify';
+import dayjs from 'dayjs';
+import DataTable from "react-data-table-component";
+import { CSVLink } from "react-csv";
+import { CopyToClipboard } from "react-copy-to-clipboard";
+import jsPDF from "jspdf";
+import "jspdf-autotable";
+import { FaCopy, FaEdit, FaFileCsv, FaFileExcel, FaFilePdf, FaTrash } from "react-icons/fa";
+import { GoSearch, GoTrashcan } from 'react-icons/go';
+import isBetween from 'dayjs/plugin/isBetween';
+import utc from 'dayjs/plugin/utc';
+import timezone from 'dayjs/plugin/timezone';
+dayjs.locale('en-au');
+dayjs.extend(isBetween);
 
 const ClientProfiles = () => {
+    dayjs.extend(utc);
+    dayjs.extend(timezone);
+    dayjs.tz.setDefault('Australia/Sydney');
   const { uid } = useParams()
   const [staffOne, setStaffOne] = useState({});
+  const [clientRoster, setClientRoster] = useState([]);
   const [profile, setProfile] = useState({})
   const [editedProfile, setEditedProfile] = useState({});
   const [loading, setLoading] = useState(false);
@@ -33,6 +50,16 @@ const ClientProfiles = () => {
       toast.error(error.response.data.message)
       toast.error(error.response.data.title)
     }
+
+    try {
+        const { data } = await get(`/ShiftRosters/get_shifts_by_user?client=${uid}&staff=`, { cacheTimeout: 300000 });
+       console.log(data);
+       setClientRoster(data.shiftRoster)
+
+    } catch (error) {
+        toast.error(error.response.data.message)
+        toast.error(error.response.data.title)
+    }
   }
   useEffect(() => {
     FetchStaff()
@@ -45,6 +72,107 @@ const ClientProfiles = () => {
       });
     }
   });
+
+  const columns = [
+    {
+        name: 'Staff',
+        selector: row => row.staff?.fullName || 'N/A',
+        sortable: true
+    },
+    // {
+    //     name: 'Client',
+    //     selector: row => row.profile.fullName,
+    //     sortable: true
+    // },
+    {
+        name: 'Date',
+        selector: row => dayjs(row.dateFrom).format('YYYY-MM-DD'),
+        sortable: true,
+    },
+    {
+        name: 'Start Time',
+        selector: row => dayjs(row.dateFrom).format('hh:mm A'),
+        sortable: true
+    },
+    {
+        name: 'End Time',
+        selector: row => dayjs(row.dateTo).format('hh:mm A'),
+        sortable: true
+    },
+    {
+        name: 'Activities',
+        selector: row => row.activities,
+        sortable: true
+    },
+    // {
+    //     name: 'DateModified',
+    //     selector: row => dayjs(row.dateModified).format('DD/MM/YYYY HH:mm:ss'),
+    //     sortable: true
+    // }
+
+];
+
+const handleExcelDownload = () => {
+    const workbook = new ExcelJS.Workbook();
+    const sheet = workbook.addWorksheet('Sheet1');
+
+    // Add headers
+    const headers = columns.map((column) => column.name);
+    sheet.addRow(headers);
+
+    // Add data
+    clientRoster.forEach((dataRow) => {
+        const values = columns.map((column) => {
+            if (typeof column.selector === 'function') {
+                return column.selector(dataRow);
+            }
+            return dataRow[column.selector];
+        });
+        sheet.addRow(values);
+    });
+
+    // Generate Excel file
+    workbook.xlsx.writeBuffer().then((buffer) => {
+        const blob = new Blob([buffer], { type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' });
+        const url = URL.createObjectURL(blob);
+        const link = document.createElement('a');
+        link.href = url;
+        link.download = 'data.xlsx';
+        link.style.visibility = 'hidden';
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+    });
+};
+
+
+const handlePDFDownload = () => {
+    const unit = "pt";
+    const size = "A4"; // Use A1, A2, A3 or A4
+    const orientation = "portrait"; // portrait or landscape
+    const marginLeft = 40;
+    const doc = new jsPDF(orientation, unit, size);
+    doc.setFontSize(13);
+    doc.text("User Table", marginLeft, 40);
+    const headers = columns.map((column) => column.name);
+    const dataValues = clientRoster.map((dataRow) =>
+        columns.map((column) => {
+            if (typeof column.selector === "function") {
+                return column.selector(dataRow);
+            }
+            return dataRow[column.selector];
+        })
+    );
+
+    doc.autoTable({
+        startY: 50,
+        head: [headers],
+        body: dataValues,
+        margin: { top: 50, left: marginLeft, right: marginLeft, bottom: 0 },
+    });
+    doc.save("Admin.pdf");
+};
+  
   const styles = {
     main: {
       backgroundColor: 'black',
@@ -195,6 +323,35 @@ const ClientProfiles = () => {
       setLoading(false)
     }
   }
+
+  const ButtonRow = ({ data }) => {
+    return (
+
+        <div className="p-2 d-flex gap-1 flex-column " style={{ fontSize: "12px" }}>
+            <div ><span className='fw-bold'>Staff: </span>{data.staff.fullName}</div>
+            <div><span className='fw-bold'>Client: </span>{data.profile.fullName}</div>
+            <div><span className='fw-bold'>Activities: </span>{data.activities}</div>
+            <div>
+                {/* <button className="btn text-info fw-bold" style={{ fontSize: "12px" }} onClick={() => handleActivityClick(data.progressNoteId)}>
+                    Edit
+                </button> */}
+            </div>
+
+        </div>
+    );
+};
+
+const [searchText, setSearchText] = useState("");
+
+    const handleSearch = (event) => {
+        setSearchText(event.target.value);
+    };
+
+    const filteredData = clientRoster.filter((item) =>
+        item.activities?.toLowerCase().includes(searchText.toLowerCase()) ||
+        item.staff?.fullName.toLowerCase().includes(searchText.toLowerCase()) ||
+        item.dateFrom?.toLowerCase().includes(searchText.toLowerCase())
+    );
 
 
   return (
@@ -766,7 +923,79 @@ const ClientProfiles = () => {
                 
               </div>
 
+              <div className='mt-4 border'>
+                        <div className="row px-2 py-3 d-flex justify-content-between align-items-center gap-4">
 
+                            <div className="col-md-3">
+                                <div className='d-flex justify-content-between border align-items-center rounded rounded-pill p-2'>
+                                    <input type="text" placeholder="Search...." className='border-0 outline-none' onChange={handleSearch} />
+                                    <GoSearch />
+                                </div>
+                            </div>
+                            <div className='col-md-5 d-flex  justify-content-center align-items-center gap-4'>
+                                <CSVLink
+                                    data={clientRoster}
+                                    filename={"document.csv"}
+
+                                >
+                                    <button
+
+                                        className='btn text-info'
+                                        title="Export as CSV"
+                                    >
+                                        <FaFileCsv />
+                                    </button>
+
+                                </CSVLink>
+                                <button
+                                    className='btn text-danger'
+                                    onClick={handlePDFDownload}
+                                    title="Export as PDF"
+                                >
+                                    <FaFilePdf />
+                                </button>
+                                <button
+                                    className='btn text-primary'
+
+                                    onClick={handleExcelDownload}
+                                    title="Export as Excel"
+                                >
+                                    <FaFileExcel />
+                                </button>
+                                <CopyToClipboard text={JSON.stringify(clientRoster)}>
+                                    <button
+
+                                        className='btn text-warning'
+                                        title="Copy Table"
+                                        onClick={() => toast("Table Copied")}
+                                    >
+                                        <FaCopy />
+                                    </button>
+                                </CopyToClipboard>
+                            </div>
+                        </div>
+                        <DataTable data={filteredData} columns={columns}
+                            pagination
+                            highlightOnHover
+                            searchable
+                            searchTerm={searchText}
+                            progressPending={loading}
+                            progressComponent={<div className='text-center fs-1'>
+                                <div className="spinner-grow text-secondary" role="status">
+                                    <span className="sr-only">Loading...</span>
+                                </div>
+                            </div>}
+                            expandableRows
+                            expandableRowsComponent={ButtonRow}
+                            paginationTotalRows={filteredData.length}
+                            responsive
+
+
+                        />
+
+                       
+
+                    </div>
             </div>
           </div>
         </div>
