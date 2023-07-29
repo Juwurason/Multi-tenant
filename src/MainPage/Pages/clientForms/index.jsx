ef48deb38836bb77578552388cea46854ed3f521
Juwurason <ajibolasunday7@gmail.com>
2023-07-29 16:20:00 +0100

    client community support added

--- src/MainPage/Pages/clientForms/index.jsx
+++ src/MainPage/Pages/clientForms/index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Redirect, Route, Switch } from 'react-router-dom';
 import ClientAidEquip from './ClientAid';
 import ClientBehaviuor from './ClientBehaviour';
+import ClientComunitySupport from './ClientComminitySupport';
 import ClientDailyLiving from './ClientDailyNightAndLiving';
 import ClientHealth from './ClientHealthSupport';
 import ClientSchedule from './ClientSchedule';
@@ -14,6 +15,7 @@ const ClientForms = ({ match }) => (
       <Route path={`${match.url}/client-daily-living/:uid`} component={ClientDailyLiving} />
       <Route path={`${match.url}/client-health/:uid`} component={ClientHealth} />
       <Route path={`${match.url}/client-behaviour/:uid`} component={ClientBehaviuor} />
+      <Route path={`${match.url}/client-community-support/:uid`} component={ClientComunitySupport} />
    </Switch>
 
 );
