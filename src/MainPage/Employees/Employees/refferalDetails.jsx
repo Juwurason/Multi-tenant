04ac2c94dee87b53e3ab58c89dedd9d26596de4a
Juwurason <ajibolasunday7@gmail.com>
2023-08-19 03:42:46 +0100

    new details adde to the table

--- src/MainPage/Employees/Employees/refferalDetails.jsx
+++ src/MainPage/Employees/Employees/refferalDetails.jsx
@@ -195,6 +195,7 @@ import useHttp from "../../../hooks/useHttp";
 import logo from "../../../assets/img/logo.png";
 import { useParams } from "react-router-dom";
 import parse from 'html-react-parser';
+import { FaBlackTie } from "react-icons/fa";
 
 const styles = StyleSheet.create({
   page: {
@@ -223,6 +224,7 @@ const styles = StyleSheet.create({
   },
   tableCell: {
     padding: 5,
+    border: "1px solid black",
     flex: 1,
     fontWeight: "normal", // Make the data less bold
     fontSize: 10, // Make the data smaller
@@ -240,6 +242,7 @@ const styles = StyleSheet.create({
     borderStyle: "solid",
     borderColor: "#000",
     borderWidth: 1,
+    borderCollapse: "collapse"
   },
   tableRow: {
     flexDirection: "row",
@@ -291,9 +294,44 @@ const RefferalDetails = () => {
             <View style={styles.table}>
               {/* Table Rows */}
               <View style={styles.tableRow}>
-                <Text style={styles.tableCell}>Name </Text>
+                <Text style={styles.tableCell}>Name:</Text>
                 <Text style={styles.tableCell}>{details.fullName}</Text>
-                
+              </View>
+              <View style={styles.tableRow}>
+                <Text style={styles.tableCell}>Email:</Text>
+                <Text style={styles.tableCell}>{details.email}</Text>
+              </View>
+              <View style={styles.tableRow}>
+                <Text style={styles.tableCell}>Current Residence:</Text>
+                <Text style={styles.tableCell}>{details.currentResidence}</Text>
+              </View>
+              <View style={styles.tableRow}>
+                <Text style={styles.tableCell}>Contact Number:</Text>
+                <Text style={styles.tableCell}>{details.contactNumber}</Text>
+              </View>
+              <View style={styles.tableRow}>
+                <Text style={styles.tableCell}>Date of Birth:</Text>
+                <Text style={styles.tableCell}>{details.dob}</Text>
+              </View>
+              <View style={styles.tableRow}>
+                <Text style={styles.tableCell}>Gender: </Text>
+                <Text style={styles.tableCell}>{details.gender}</Text>
+              </View>
+              <View style={styles.tableRow}>
+                <Text style={styles.tableCell}>Do they identify as Aboriginal or Torres Strait Islander?: </Text>
+                {/* <Text style={styles.tableCell}>{details.gender}</Text> */}
+              </View>
+              <View style={styles.tableRow}>
+                <Text style={styles.tableCell}>Do they identify as Culturally and LinguisticallyDiverse(CALD)? </Text>
+                {/* <Text style={styles.tableCell}>{details.gender}</Text> */}
+              </View>
+              <View style={styles.tableRow}>
+                <Text style={styles.tableCell}>NOK/ EmergencyContact Details: </Text>
+                {/* <Text style={styles.tableCell}>{details.gender}</Text> */}
+              </View>
+              <View style={styles.tableRow}>
+                <Text style={styles.tableCell}>Do they havea nominated carer?If yes, please providecontact details </Text>
+                {/* <Text style={styles.tableCell}>{details.gender}</Text> */}
               </View>
             </View>
           </View>
